3dcab77acb55ea1788d01b472fc7576bc884cac1
flabbr <logical.pickle.boy@gmail.com>
2023-09-29 15:44:28 -0600

    New changes

--- scripts/color-clicker.js
+++ scripts/color-clicker.js
@@ -1,8 +1,3 @@
-// "this" does
-// funtions are basically methods outside a class
-//method does something in a class and properties are characteristics within a class
-// constructor is what you do to build the class
-// to get started, every class needs a constructor
 //@ts-check
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
@@ -13,24 +8,49 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
+/** @type {HTMLCanvasElement} */
+//@ts-ignore canvas is an HTMLCanvasElement
+const scoreCanvas = document.getElementById("score-canvas");
+/** @type {CanvasRenderingContext2D} */
+//@ts-ignore we know ctx is not null
+const scoreCtx = scoreCanvas.getContext("2d");
+scoreCanvas.width = 800;
+scoreCanvas.height = 100;
+
 class ClickShape {
-	constructor() {
+	/**
+	 * @param {CanvasRenderingContext2D} [ctx]
+	 */
+	constructor(ctx) {
+		/**
+	/** @type {CanvasRenderingContext2D}*/
+		//@ts-ignore
+		this.ctx = ctx;
+
 		this.x = 0;
 		this.y = 0;
-		this.width = 10;
-		this.xDirection = 1;
+
+		this.width = 100;
+
+		this.xDirection = 0;
 		this.yDirection = 1;
+
 		this.speed = 5;
+
 		this.isVisible = true;
-		this.isClicked = true;
+		this.isClicked = false;
 
 		this.color = "silver";
+
+		/** @type {Path2D | undefined} */
+		this.path;
 	}
 
 	movingDirection() {
-		if (this.xDirection === 0 && this.yDirection === 0) {
+		if (this.xDirection === 0 && this.yDirection === 1) {
 			return "down";
 		}
+
 		if (this.xDirection === 0 && this.yDirection === -1) {
 			return "up";
 		}
@@ -44,7 +64,7 @@ class ClickShape {
 
 	update() {
 		if (!this.isVisible) {
-			return; //leave this method, nothing to do
+			return; // leave this method, nothing to do
 		}
 
 		switch (this.movingDirection()) {
@@ -61,17 +81,47 @@ class ClickShape {
 				this.isVisible = this.x + this.width > 0;
 				break;
 		}
+
 		this.x += this.xDirection * this.speed;
-		this.x += this.yDirection * this.speed;
+		this.y += this.yDirection * this.speed;
 	}
-	draw() {
-		ctx.fillStyle = this.color;
-		ctx.fillRect(this.x, this.y, this.width, this.width);
+
+	draw() {}
+
+	checkForClicked(x, y) {
+		if (this.isClicked) {
+			return; // leave if we are clicked so we can't be un-clicked
+		}
+
+		//@ts-ignore path will not be undefined here
+		this.isClicked = ctx.isPointInPath(this.path, x, y);
+		console.log(this.path, x, y, this.x, this.y);
+		//this.color = "silver";
 	}
 }
+
+class CircleClickShape extends ClickShape {
+	/**
+	 * @param {CanvasRenderingContext2D} [ctx]
+	 */
+	constructor(ctx) {
+		super(ctx);
+	}
+	draw() {
+		this.ctx.fillStyle = this.isClicked ? "silver" : this.color;
+
+		this.path = new Path2D();
+		//this.path.rect(this.x, this.y, this.width, this.width);
+		this.path.arc(this.x, this.y, this.width / 2, 0, Math.PI * 2);
+		this.ctx.fill(this.path);
+	}
+}
+
 class Game {
 	constructor() {
 		this.score = 0;
+		this.isGameOver = false;
+
 		this.colors = [
 			"red",
 			"orange",
@@ -82,58 +132,123 @@ class Game {
 			"violet",
 		];
 
-		this.targetColor = this.getRandomColor();
+		this.targetShape = this.getRandomTargetShape();
+
 		/**@type {Array<ClickShape>} */
 		this.shapes = [];
-		this.spawnInterval = 500; //milliseconds
+
+		this.spawnInterval = 500; // milliseconds
 		this.lastSpawnTime = 0;
+	}
+	getRandomTargetShape() {
+		let s = new ClickShape(scoreCtx);
+		s.color = this.getRandomColor();
+		s.width = scoreCanvas.height * 0.8;
+		s.x = scoreCanvas.width / 2 - s.width / 2;
+		s.y = 5;
+		return s;
 	}
 
 	getRandomColor() {
 		let randomIndex = Math.floor(Math.random() * this.colors.length);
 		return this.colors[randomIndex];
 	}
-	spawnShape(elapsedTime) {
 
-this.lastSpawnTime += elapsedTime;
-if(this.lastSpawnTime <= this.spawnInterval) {
-	return; //lave, not enough time since last spawn
-}
-// reset our LastSpawnTime
-this.lastSpawnTime = 0;
-let s = new ClickShape();
-s.color =  this.getRandomColor();
-s.y = 0-s.width;
-//this will give a number between 1 and 70
-let randX = Math.floor(
-	Math.random() * (canvas.width / s.width) - s.width);
-s.x =  randX * s.width
-//push the new shape into the array
-this.shapes.push(s);	
+	spawnShape(elapsedTime) {
+		this.lastSpawnTime += elapsedTime;
+		if (this.lastSpawnTime < this.spawnInterval) {
+			return; // leave, not enough time since last spawn
+		}
+
+		// console.log("in spawnShape");
+
+		// reset our last spawn time
+		this.lastSpawnTime = 0;
+
+		let s = new ClickShape(ctx);
+		s.color = this.getRandomColor();
+		s.y = 0 - s.width;
+
+		let randX = Math.floor(Math.random() * (canvas.width / s.width));
+
+		s.x = randX * s.width;
+
+		//console.log(s);
+
+		// push the new shape into our array
+		this.shapes.push(s);
 	}
 
 	update(elapsedTime) {
 		this.spawnShape(elapsedTime);
-		this.shapes.forEach((s) = {s.update();})
+
+		this.shapes.forEach((s) => {
+			s.update();
+		});
+
+		this.shapes = this.shapes.filter((s) => s.isVisible);
+
+		// console.log("length of shapes array", this.shapes.length);
+	}
+
+	draw() {
+		this.shapes.forEach((s) => {
+			s.draw();
+		});
+		scoreCtx.font = "60px JetBrainsMono";
+		scoreCtx.fillStyle = "red";
+		scoreCtx.fillText(`Score: ${this.score}`, 0, 55);
+		this.targetShape.draw();
+	}
+	checkForClicked(x, y) {
+		this.shapes.forEach((s) => {
+			s.checkForClicked(x, y);
+		});
+
+		let clickedShapes = this.shapes.filter((s) => s.isClicked);
+		// console.log(clickedShapes);
+		// debugger;
+
+		if (clickedShapes.length === 0) {
+			return;
+		}
+
+		// get the last shape drawn that was clicked and see if it
+		// matches our target color
+		if (
+			clickedShapes[clickedShapes.length - 1].color ===
+			this.targetShape.color
+		) {
+			this.score++;
+			this.targetShape = this.getRandomTargetShape();
+		} else {
+			this.isGameOver = true;
+		}
 	}
 }
 
-
 let game = new Game();
-console.log(game);
+
+//console.log(game);
 
 let currentTime = 0;
 
-let gameLoop = function(timestamp) {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+let gameLoop = function (timestamp) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	scoreCtx.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
 
-let elapsedTime = timestamp - currentTime;
-currentTime = timestamp;
+	let elapsedTime = timestamp - currentTime;
+	currentTime = timestamp;
 
-game.update(elapsedTime);
-game.draw();
+	game.update(elapsedTime);
+	game.draw();
 
-window.requestAnimationFrame(gameLoop);
+	window.requestAnimationFrame(gameLoop);
 };
 
 window.requestAnimationFrame(gameLoop);
+
+canvas.addEventListener("click", (ev) => {
+	console.log("mouse event", ev.offsetX, ev.offsetY);
+	game.checkForClicked(ev.offsetX, ev.offsetY);
+});
